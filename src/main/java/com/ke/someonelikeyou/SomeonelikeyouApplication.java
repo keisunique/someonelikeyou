c6286d7445505a4607f3dd04efcfd608e6f443d8
zhengrongke <zhengrongke@vcmy.com>
2017-11-15 19:33:51 +0800

    first

---
+++ src/main/java/com/ke/someonelikeyou/SomeonelikeyouApplication.java
@@ -0,0 +1,12 @@
+package com.ke.someonelikeyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeonelikeyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeonelikeyouApplication.class, args);
+	}
+}
